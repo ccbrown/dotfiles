bee81a1804495d6c2ea058d849f438796246bbb9
Christopher Brown <cbrown@bittorrent.com>
2016-11-04 02:10:35 -0700

    fix hot corner typo

--- macos/up.sh
+++ macos/up.sh
@@ -166,9 +166,9 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 # Top right screen corner → Desktop
 defaults write com.apple.dock wvous-tr-corner -int 4
 defaults write com.apple.dock wvous-tr-modifier -int 0
-# Bottom left screen corner → Put display to sleep
-defaults write com.apple.dock wvous-bl-corner -int 10
-defaults write com.apple.dock wvous-bl-modifier -int 0
+# Bottom right screen corner → Put display to sleep
+defaults write com.apple.dock wvous-br-corner -int 10
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 ###############################################################################
 # Safari & WebKit                                                             #
