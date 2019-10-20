6aa0f27861841be0bacaa34e1ec0d4a976960742
Christopher Brown <chris@Christophers-MacBook-Pro.local>
2019-10-20 10:52:21 -0400

    gitconfig and gitignore

--- git/up.sh
+++ git/up.sh
@@ -2,5 +2,5 @@
 DIR=$(cd "$(dirname "$0")" ; pwd -P)
 
 rm -f $HOME/.zshrc.d/git.zsh && ln -s $DIR/.zshrc $HOME/.zshrc.d/git.zsh
+cp .gitignore ~/.gitignore
 git config --global include.path $DIR/.gitconfig
-
